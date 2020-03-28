43a967c196c666feca6af8d63d18ba3f7a520ceb
vladislavvasyukov <vladislav.vasyukof@gmail.com>
2020-03-28 13:27:03 +0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# C-How-to-Program
+Solutions to "C++ How to Program ( by Deitel &amp; Deitel 8th edition)"
---
+++ chapter_2/2-16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "Enter two integers:" << endl;
+	cin >> x >> y;
+	cout << x + y << endl;
+	cout << x * y << endl;
+	cout << x - y << endl;
+	cout << x / y << endl;
+}
